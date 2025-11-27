b8c66669d2fa6a20b3318efedcdf6140c8db2de2
analivia05 <ana.silva5909@etec.sp.gov.br>
2025-11-27 12:25:49 -0300

    27/11

--- aulaBD/curso_cadastro.php
+++ aulaBD/curso_cadastro.php
@@ -2,23 +2,34 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cadastro de Cursos</title>
+    <link rel="stylesheet" href="estilos.css">
 </head>
 <body>
-    <h1> Cadastro de Cursos </h1>
-    <form name="Enviar" method="POST" action="curso_cadastro.php">
-        <fieldset>
-            <p> Informe o nome do curso: </p>
-            <input type="text" name="curso">
 
-            <p> Informe o nome do coordenador: <p>
+<?php include "menu.php" ?>
+   <div class="conteudo">
+
+    <h3> Cadastro de Cursos </h3>
+    <form method="post" action="curso_cadastro1.php">
+
+            <p>Informe o nome do curso:<br>
+            <input type="text" name="nome">
+            </p>
+            <p>Informe o nome do coordenador:<br>
             <input type="text" name="coordenador">
-            <br>
-            <br>
-            <input type="submit" name="Enviar" value="Cadastrar Curso">
-</fieldset>
+            </p>
 
-</body>
-</html>
+            
+            <p>
+
+            <input type="submit" name="enviar" value="Cadastrar"> 
+            </p>
+    </form>
+</div>
+
+<body>
+    <html>
+
+          
